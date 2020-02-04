5c4c3d06e2cb8b8ef01d693459516bb9d65fe7ac
Adeverdauss <ade.10rpl1.2019@gmail.com>
2020-02-04 20:11:51 +0700

    login

--- app/src/main/java/com/addfray/login/Kalkulator.java
+++ app/src/main/java/com/addfray/login/Kalkulator.java
@@ -3,15 +3,70 @@ package com.addfray.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Kalkulator extends AppCompatActivity {
 
-
+ EditText Angka1, Angka2;
+ Button btn1, btn2, btn3, btn4;
+ TextView Hasil;
+ Float Result;
+ float angka1, angka2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulator);
 
+        Angka1 = findViewById(R.id.angka1);
+        Angka2 = findViewById(R.id.angka2);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        Hasil = findViewById(R.id.tv);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angka1=Float.parseFloat(Angka1.getText().toString());
+                angka2=Float.parseFloat(Angka2.getText().toString());
+                Result=angka1+angka2;
+                Hasil.setText(Result+"");
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angka1=Float.parseFloat(Angka1.getText().toString());
+                angka2=Float.parseFloat(Angka2.getText().toString());
+                Result=angka1-angka2;
+                Hasil.setText(Result+"");
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angka1=Float.parseFloat(Angka1.getText().toString());
+                angka2=Float.parseFloat(Angka2.getText().toString());
+                Result=angka1*angka2;
+                Hasil.setText(Result+"");
+
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angka1=Float.parseFloat(Angka1.getText().toString());
+                angka2=Float.parseFloat(Angka2.getText().toString());
+                Result=angka1/angka2;
+                Hasil.setText(Result+"");
+
+            }
+        });
     }
 }
--- app/src/main/java/com/addfray/login/MainActivity.java
+++ app/src/main/java/com/addfray/login/MainActivity.java
@@ -5,42 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText e1, e2;
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtusername = findViewById(R.id.editText1);
+        txtpassword = findViewById(R.id.editText2);
+        btnlogin = findViewById(R.id.button);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtusername.getText().toString().isEmpty()|| txtpassword.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Harus Terisi Semua", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (txtusername.getText().toString().equals("ade") && txtpassword.getText().toString().equals("ade")) {
+                        Toast.makeText(MainActivity.this, "Login Succesful", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(MainActivity.this, Kalkulator.class );
+                        startActivity(i);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
     }
 
-    public void btn_click(View V)
-    {
-            e1 =(EditText) findViewById(R.id.editText1);
-            e2 =(EditText) findViewById(R.id.editText2);
 
-            String uname = e1.getText().toString();
-            String pass  = e2.getText().toString();
 
-            if (e1.getText().toString().isEmpty()|| e2.getText().toString().isEmpty()){
-            Toast.makeText(MainActivity.this, "Harus Terisi Semua", Toast.LENGTH_SHORT).show();
-    }else {
-                if (uname.equals("Patrick") && pass.equals("hello")) {
-                Toast.makeText(this, "Login Succesful", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(MainActivity.this, Kalkulator.class );
-                i.putExtra("userName", uname);
-                startActivity(i);
-            }
-            else {
-                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
-            }
 
-            }
-
-    }
 }
